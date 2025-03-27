3f2e1c57e6cde3bffe83b6db1d8e12d7c1656ba3
Khoi Hoang <khoisyhoang@gmail.com>
2025-03-26 22:02:06 -0400

    integrated pug and nodemon

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
+const path = require('path');
 const port = 3000
+const app = express()
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
 
 app.get('/', (req, res) => {
-  res.send('Main page')
+  res.render("client/pages/home")
 })
+
+app.get('/tours', (req, res) => {
+  res.render("client/pages/tour-list", {
+    a: 10,
+    b: 20
+  })
+})
+
 app.get('/tours', (req, res) => {
   res.send('Tour lists!')
 })
